6bb1cce11848f0c29c05ed3bfad1c3834c9c6dc5
csguterres <cleissonsg@gmail.com>
2021-04-12 02:17:48 -0300

    busca por musica e sugestao de musica consertado

--- musicplus/src/main/java/br/ufes/informatica/musicplus/core/domain/Album_.java
+++ musicplus/src/main/java/br/ufes/informatica/musicplus/core/domain/Album_.java
@@ -7,11 +7,11 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2021-04-11T16:41:53.641-0300")
+@Generated(value="Dali", date="2021-04-12T01:52:47.050-0300")
 @StaticMetamodel(Album.class)
 public class Album_ extends PersistentObjectSupport_ {
 	public static volatile SingularAttribute<Album, String> nome;
 	public static volatile SingularAttribute<Album, Date> dataLancamento;
-	public static volatile SetAttribute<Album, Musica> musicas;
 	public static volatile SingularAttribute<Album, Artista> artista;
+	public static volatile SetAttribute<Album, Musica> musicas;
 }
